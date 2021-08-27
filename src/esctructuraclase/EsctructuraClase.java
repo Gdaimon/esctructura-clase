6efee611f717b76fb1fb9334670c7b8656db85a0
Carlos Andres Charris S <darkklitos@gmail.com>
2021-08-26 22:31:11 -0500

    Esctructura Clase

---
+++ src/esctructuraclase/EsctructuraClase.java
@@ -0,0 +1,12 @@
+package esctructuraclase;
+
+public class EsctructuraClase {
+
+    public static void main(String[] args) {
+        int numero = 6;
+        Lapiz lapiz = new Lapiz();
+        lapiz.pintar();
+        System.out.println(lapiz.color);
+    }
+    
+}
---
+++ src/esctructuraclase/Lapiz.java
@@ -0,0 +1,12 @@
+package esctructuraclase;
+
+class Lapiz {
+    String color= "Amarillo"; // Atributos
+    int tamanoCentrimetros = 10; // Atributo
+    String material ="Madera";
+    
+    // Métodos
+    void pintar(){
+        System.out.println("Pintar sobre algo...!");
+    }
+}
